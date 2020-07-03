72b6505709eac9086aeedacf6d0202860e4dccb4
Krishnendu <kdas081989@gmail.com>
2020-07-04 00:22:17 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+	@Test
+	public void NativeAndroid()
+	{
+		System.out.println("This is NativeAndroid");
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+}
---
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+
+	@Test
+	public void postJira()
+	{
+		System.out.println("PostJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
